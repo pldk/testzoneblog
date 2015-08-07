c76bd2ffe508f033a1e5aa68fd33898db24beb33
Philippe de Liedekerke <philippe.deliedekerke@gmail.com>
2015-08-07 17:10:05 +0200

    comments nested

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,28 @@
 class CommentsController < ApplicationController
 
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
+
+
+  def show
+    @article = Article.find(params[:id])
+    @new_comment = @article.comments.build
+  end
+
+
+
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comments_params)
+    @comment = @article.comments.create(comment_params)
 
     redirect_to article_path(@article)
   end
 
+
+
   def destroy
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
+    @comment = @article.comments.create(comment_params)
     @comment.destroy
 
     redirect_to article_path(@article)
